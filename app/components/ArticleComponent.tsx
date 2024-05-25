e3068a6a8db5d06f7ab9dd6590809633e1c232cd
MookZikLab <165441047+MookZikLab@users.noreply.github.com>
2024-05-25 16:24:34 +0900

    qa

--- app/components/ArticleComponent.tsx
+++ app/components/ArticleComponent.tsx
@@ -12,7 +12,7 @@ const ArticleComponent = ({articleNumber = 1, type = "main", visibleContents = t
     <Link href={`/article/${articleNumber}`} style={{textDecoration: 'none', borderBottom: `${type === "main" ? "none" : "solid 1px #DEE2E6"}` }}>
       <div className={"title"} style={{height: `${type === "main" ? "60px" : "36px"}`, padding: "10px 0", display: 'flex', alignItems: "center"}}>
         <h2 style={{fontWeight: 600, color: "#343A40", fontSize : `${type === "main" ? "40px" : "24px"}`, overflow:"hidden", textOverflow:"ellipsis", whiteSpace:"nowrap",}}>
-          제목 zczczxczczxczxczc xzxczczxczxczxczxczxcz xczczxczxczcxzzxczxczxczcz
+          尹 대통령 김치찌개 만찬 “언론인 해외 연수, 내년부터 세 자리로”
         </h2>
       </div>
       <div className={"title-image"} style={{width: "100%", height: `${types.get(type).height ?? 300}px`, position:"relative"}}>
@@ -36,8 +36,7 @@ const ArticleComponent = ({articleNumber = 1, type = "main", visibleContents = t
             WebkitBoxOrient: 'vertical',
             WebkitLineClamp: '2'
           }}>
-            content zczczxczczxczxczcxzxczczxcz xczxczxczxczxczczxczxczcxzzxczxczxcz czzczczxczczxczxczcxzxczczx
-            czxczxczxczxczxczczxczxczcxzzxczxczxczcz
+            윤석열 대통령이 박근혜 대통령 최측근으로 ‘문고리 3인방’ 중 한 명인 정호성 전 청와대 부속비서관을 대통령실 시민사회수석실 3비서관으로 기용했다. 정 비서관은 박 전 대통령 지시로 청와대 기밀 문건을 최순실씨에게 유출한 혐의 등으로 1년 6개월을 복...
           </div>
         }
       </div>
